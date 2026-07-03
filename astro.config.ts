127fbf9f4fcc7b925510f28e46edbd50272053ca
probablyduncan <duncanpetrie1@gmail.com>
2026-07-03 14:25:50 +0100

    falafel.fyi domain

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import generateStandardSiteWellKnownFile from "./src/lib/generateStandardSiteWellKnownFile";
 
 export default defineConfig({
-    site: "https://falafel.duncanpetrie.com",
+    site: "https://falafel.fyi",
     integrations: [
         generateStandardSiteWellKnownFile(),
     ]
