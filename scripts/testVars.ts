18a692e45755a1e80461a51e86ae0d8ebe121fe4
probablyduncan <duncanpetrie1@gmail.com>
2026-07-03 16:55:30 +0100

    update script

--- scripts/testVars.ts
+++ scripts/testVars.ts
@@ -1,3 +1,5 @@
 // console.log(process.env)
 console.log("heyyy")
-console.log("service:", process.env.ATPROTO_SERVICE)
+console.log("service:", process.env.ATPROTO_SERVICE?.replaceAll("https", ""))
+console.log("is it equal to hardcoded val?", process.env.ATPROTO_SERVICE === "https://bsky.social")
+console.log("hardcoded", "https://bsky.social");
