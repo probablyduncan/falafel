d886b800ce25d4e8332e3b573162b0579abb415e
Duncan Petrie <57732343+probablyduncan@users.noreply.github.com>
2026-07-07 15:30:42 +0100

    never show migration date

--- src/lib/dateHelpers.ts
+++ src/lib/dateHelpers.ts
@@ -3,14 +3,31 @@ import type { FalafelPlace } from "./falafelFetcher";
 export const getTimestamp = (dateString: string) => new Date(dateString).getTime();
 export const MIGRATION_CUTOFF_TIME = getTimestamp("2026-03-06");
 
-export function getDateEatenText(p: Pick<FalafelPlace, "dateEatenText" | "dateEaten" | "dateSaved">) {
+export function getDateEatenText(p: Pick<FalafelPlace, "dateEatenText" | "dateEaten" | "dateSaved">, fallback: string = "[unknown]"): string {
     if (p.dateEatenText) {
         return p.dateEatenText;
     }
 
+    const dateEaten = new Date(p.dateEaten);
+    if (dateEaten?.getTime()) {
+        return getFormattedDate(dateEaten);
+    }
+
+    const dateSaved = new Date(p.dateSaved);
+    if (dateSaved.getTime() > MIGRATION_CUTOFF_TIME) {
+        return getFormattedDate(dateSaved);
+    }
+    
+    return fallback;
+}
+
+function getFormattedDate(date: Date | undefined): string {
+    if (!date) {
+        return "";
+    }
     return new Intl.DateTimeFormat("en-US", {
         year: "numeric",
         day: "2-digit",
         month: "short",
-    }).format(new Date(p.dateEaten ? p.dateEaten : p.dateSaved));
+    }).format(date);
 }
