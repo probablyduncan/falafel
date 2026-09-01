c9934c0a45ef0f2ac73bde28bbcfb64a80337a1e
probablyduncan <duncanpetrie1@gmail.com>
2026-09-01 11:26:01 +0100

    fix infinite loop, some style tweaks, add ordinal fn

--- src/lib/textHelpers.ts
+++ src/lib/textHelpers.ts
@@ -14,3 +14,22 @@ export function countWords(text: string) {
     return text?.trim().split("\n").flatMap(p => p.split("—")).flatMap(p => p.split("-")).flatMap(p => p.split(" ")).filter(Boolean).length ?? 0;
 }
 
+export function getOrdinal(number: number) {
+
+    const last = number % 10;
+    const lastTwo = number % 100;
+    
+    if (last === 1 && lastTwo !== 11) {
+        return "st";
+    }
+
+    if (last === 2 && lastTwo !== 12) {
+        return "nd";
+    }
+
+    if (last === 3 && lastTwo !== 13) {
+        return "rd";
+    }
+
+    return "th";
+}
